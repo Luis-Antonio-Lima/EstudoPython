9ba08fbb23783e7057ccc55b5aeeb06c34e16c04
Luis Antonio Lima <nsei06199@gmail.com>
2024-02-29 15:31:33 -0300

    Init: iniciando o estudo em Python. Exibindo mensagens

--- Aula01/boasvindas.py
+++ Aula01/boasvindas.py
@@ -2,5 +2,5 @@
 nome = "admin"
 print("Olá, "+nome+". Seja bem vindo!")
 print("Escreva o seu novo nome de usuário:")
-nome + input("Digite aqui:")
+nome = input("Digite aqui:")
 print("O seu novo usuário é: "+nome)
