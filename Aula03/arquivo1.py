d417f43e0d479c290601c5cfe61ea901fc43746c
Luis Antonio Lima <nsei06199@gmail.com>
2024-03-05 16:04:53 -0300

    Trabalhando com arquivos em python

---
+++ Aula03/arquivo1.py
@@ -0,0 +1,8 @@
+# Open é uma função que permite abrir o arquivo, ler ou escrever neste arquivo
+arq = open("dados.txt","a")
+
+# A função write, permite escrever emum arquuivo
+arq.write("Olá\n")
+
+# A função close fecha o arquivo retirando-o da memória
+arq.close()
